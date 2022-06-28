8c6e0deb6209da3a6948569297bfbae46bf95400
hidao80 <8155294+hidao80@users.noreply.github.com>
2022-06-29 06:14:50 +0900

    add: Open feed in a separate tab

--- js/main.js
+++ js/main.js
@@ -92,7 +92,7 @@ function drawFeeds(url) {
             let rssTitle = rss[i].getElementsByTagName("title")[0].textContent;
             let rssLink = rss[i].getElementsByTagName("link")[0].textContent;
 
-            const tagString = `<a href="${rssLink}">${rssTitle}</a><br/>`;
+            const tagString = `<a target="_blank" href="${rssLink}">${rssTitle}</a><br/>`;
 
             cardBody.insertAdjacentHTML('beforeend', tagString);
         }
